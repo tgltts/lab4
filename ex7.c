7bec558c4720411e64b97fd51ce90fedc74fdfc5
tgltts <g82651504@gmail.com>
2025-09-02 22:01:27 -0300

    Update ex7.c

--- ex7.c
+++ ex7.c
@@ -1,38 +1,34 @@
-#include <stdion.h>
+#include <stdio.h>    
 #include <stdlib.h>
 #define TAM 5
 
-int buscaValor(int vetor[];intx){
-  for (int i=0; i < 5; i++) {
-      if (vetor[i] == x ){
-          return i;
-      }
-  }
-  return -1
+int buscaValor(int vetor[], int x) {   
+    for (int i = 0; i < TAM; i++) {
+        if (vetor[i] == x) {
+            return i;
+        }
+    }
+    return -1;   
 }
 
-int main(){
-  int vetor[5];
-  int x;
+int main() {
+    int vetor[TAM];
+    int x;
 
-  printf("Digite 5 números inteiros: \n");
-  for ( i=0 ; i<5; i++){
-      scanf("%d", ¨&vetor[i]);
-  }
+    printf("Digite 5 números inteiros: \n");
+    for (int i = 0; i < TAM; i++) {   
+        scanf("%d", &vetor[i]);       
+    }
 
-  printf("Digite o valor que deseja procurar: ");
-  scanf("%d" , &x);
+    printf("Digite o valor que deseja procurar: ");
+    scanf("%d", &x);
 
-  int posicao = buscaValor(vetor,x);
+    int posicao = buscaValor(vetor, x);
 
-  if (posicao != -1){
-      printf("O valor %d esta na posição %d do vetor \n, x, posicao)
-   } 
-        else { 
-        printf("O valor %d nao foi encontrado no vetor \n", x)
-        }
+    if (posicao != -1) {
+        printf("O valor %d está na posição %d do vetor \n", x, posicao);  
+    } else {
+        printf("O valor %d não foi encontrado no vetor \n", x);         
+    }
     return 0;
-  }
-  
-
-  
+}
